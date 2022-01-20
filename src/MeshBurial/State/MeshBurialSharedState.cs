60009939ddaa87edd346318fd447d5c5e1b93254
Chris Schubert <chris.a.schubert25@gmail.com>
2022-01-20 13:05:24 -0500

    Finishing FSW architecture, Integrating DOTween as source library

--- src/MeshBurial/State/MeshBurialSharedState.cs
+++ src/MeshBurial/State/MeshBurialSharedState.cs
@@ -19,11 +19,13 @@ namespace Appalachia.Spatial.MeshBurial.State
     {
         static MeshBurialSharedState()
         {
-            
-            
             RegisterDependency<MeshBurialGizmoSettings>(i => _meshBurialGizmoSettings = i);
             RegisterDependency<MeshBurialOptimizationParameters>(i => _meshBurialOptimizationParameters = i);
             MeshObjectManager.InstanceAvailable += i => _meshObjectManager = i;
+        }
+
+        public MeshBurialSharedState()
+        {
         }
 
         public MeshBurialSharedState(GameObject instanceOrPrefab, MeshBurialOptimizationParameters op) : base(
